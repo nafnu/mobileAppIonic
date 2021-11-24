bec0664c1be7cd48c43f98b9191f33705b0ce28d
nathalie <nafnu@hotmail.com>
2021-11-24 17:06:41 +0000

    ionic config

---
+++ ionic/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'dorset-team-3-ticketing',
+    appId: '1:257557406062:web:2f8639a94a51a85f27a17f',
+    storageBucket: 'dorset-team-3-ticketing.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAEmIXNXcoahBhDk9WMBHXEdZ3XmaG9jeU',
+    authDomain: 'dorset-team-3-ticketing.firebaseapp.com',
+    messagingSenderId: '257557406062',
+    measurementId: 'G-LMZMWNNRZY',
+  },
+  production: true
+};
--- ionic/src/environments/environment.ts
+++ ionic/src/environments/environment.ts
@@ -3,15 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
   firebase: {
     apiKey: "AIzaSyBMcVNELjPHT67NDkJhd_4tdOxncjuB_Jk",
     authDomain: "team3-booking-system.firebaseapp.com",
     projectId: "team3-booking-system",
     storageBucket: "team3-booking-system.appspot.com",
     messagingSenderId: "356504014450",
-    appId: "1:356504014450:web:624a9b363db41dae7fbceb"
-  }
+    appId: "1:356504014450:web:624a9b363db41dae7fbceb",
+    measurementId: "G-WMBXS2Q32E"
+  },
+  production: false
 };
 
 /*
